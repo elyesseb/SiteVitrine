18fedeec967d9d391642befa2ac394f970ea0bb3
DESKTOP-R7U4PP7\ufhopla <elyesse.benhadj@gmail.com>
2021-05-24 22:41:03 +0200

    Improve

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,17 +1,24 @@
- // core version + navigation modules:
- import Swiper from 'swiper';
- import SwiperCore, { Navigation } from 'swiper/core';
- // import Swiper styles
- import 'swiper/swiper-bundle.css';
- // configure Swiper to use modules
- SwiperCore.use([Navigation]);
- // init Swiper:
- const swiper = new Swiper(".mySwiper", {
-     // Optional parameters
-     loop: true,
-     // Navigation arrows
-     navigation: {
-         nextEl: ".swiper-button-next",
-         prevEl: ".swiper-button-prev",
-     },
- });
+  // import Swiper bundle with all modules installed
+  import Swiper from 'swiper/bundle';
+  // import Swiper styles
+  import 'swiper/swiper-bundle.css';
+  // init Swiper:
+  const swiper = new Swiper(".mySwiper", {
+      // Optional parameters
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+          delay: 3500,
+          disableOnInteraction: true,
+      },
+      // pagination parameters
+      pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+      },
+      // Navigation arrows
+      navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+      },
+  });
